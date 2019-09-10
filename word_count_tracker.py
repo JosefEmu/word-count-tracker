ff8d3bfd09573f9c210630b90a6dc4cc2ce90546
Josef Emu <41541801+JosefEmu@users.noreply.github.com>
2019-09-10 14:50:30 +0300

    Update word_count_tracker.py

--- word_count_tracker.py
+++ word_count_tracker.py
@@ -3,6 +3,8 @@ import docx2txt, subprocess, time
 # Getting the docx document
 MY_TEXT = docx2txt.process(input("Enter the full URL to your .docx here: "))
 TIMER = 300
+TARGET_ = input("What is your target word count?: ")
+
 def wordcount():	
 	#copying text from .docx to .txt file 
 	with open("output_file.txt", "w") as f:
@@ -13,11 +15,11 @@ def wordcount():
 		words = [word for line in text_file for word in line.strip().split()]
 		current_word_count = (len(words))
 
-	if current_word_count>3000:
+	if current_word_count>TARGET_:
 		print("Done!")
 		quit()
 	else:
-		print(f"\nCurrent word count is: {current_word_count} words.\nOnly {3000-current_word_count} words left to go...\n")
+		print(f"\nCurrent word count is: {current_word_count} words.\nOnly {TARGET_-current_word_count} words left to go...\n")
 
 def run_it():
 	while True:
