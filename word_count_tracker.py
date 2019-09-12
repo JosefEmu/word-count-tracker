a615b1e648c23d60d2b817a667a36dafe6bedadf
Josef Emu <41541801+JosefEmu@users.noreply.github.com>
2019-09-12 10:04:57 +0300

    Update word_count_tracker.py

--- word_count_tracker.py
+++ word_count_tracker.py
@@ -1,25 +1,33 @@
 import docx2txt, subprocess, time
 
 # Getting the docx document
-MY_TEXT = docx2txt.process(input("Enter the full URL to your .docx here: "))
-TIMER = 300
-TARGET_ = input("What is your target word count?: ")
+print("Welcome\n")
+MY_DOCX = docx2txt.process(input("Enter the full URL to your .docx here: "))
+
+change_timer = input("\nWould you like to change the timer from the default 30 seconds interval? Y/N: ")
+if change_timer != "Y":
+	TIMER = 30
+
+else:
+	TIMER= int(input("\nInput new timer in seconds: "))
+
+TARGET_ = int(input("\nWhat is your target word count?: "))
 
 def wordcount():	
 	#copying text from .docx to .txt file 
 	with open("output_file.txt", "w") as f:
-	    print(MY_TEXT, file=f)
+	    print(MY_DOCX, file=f)
 
 	#getting the word count (less characters in MS word)
 	with open("output_file.txt") as text_file:
 		words = [word for line in text_file for word in line.strip().split()]
 		current_word_count = (len(words))
-
+	
 	if current_word_count>TARGET_:
 		print("Done!")
 		quit()
 	else:
-		print(f"\nCurrent word count is: {current_word_count} words.\nOnly {TARGET_-current_word_count} words left to go...\n")
+		print(f"\nCurrent word count is: {current_word_count} words.\nOnly {TARGET_-current_word_count} words left to go...\n\n---\n")
 
 def run_it():
 	while True:
@@ -27,3 +35,4 @@ def run_it():
 		time.sleep(TIMER)
 		subprocess.call("cls", shell=True)
 		subprocess.call("exit", shell=True)
+run_it()
